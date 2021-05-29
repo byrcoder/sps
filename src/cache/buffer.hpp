f98bec9852c8f49bc275a983360e6e2ed68d907a
byrcoder <byrcoder@0712@gmail.com>
2021-05-30 01:13:10 +0800

    add: cache

---
+++ src/cache/buffer.hpp
@@ -0,0 +1,52 @@
+/* Copyright [2020] <Tencent, China>
+ *
+ * =====================================================================================
+ *        Version:  1.0
+ *        Created:  on 2021/5/30.
+ *        Author:  weideng(邓伟).
+ *
+ * =====================================================================================
+ */
+#ifndef SPS_BUFFER_HPP
+#define SPS_BUFFER_HPP
+
+#include <memory>
+
+namespace sps {
+
+class IBuffer {
+ public:
+    virtual ~IBuffer() = default;
+
+ public:
+    virtual char *buffer() = 0;
+
+    virtual int size() = 0;
+};
+
+typedef std::shared_ptr<IBuffer> PIBuffer;
+
+class CharBuffer;
+typedef std::shared_ptr<CharBuffer> PCharBuffer;
+
+class CharBuffer : public IBuffer {
+ public:
+    static PCharBuffer copy(char* buf, int len);
+    // static  __unused PCharBuffer nocopy(char* buf, int len);
+
+ public:
+    CharBuffer(char* buf, int len);
+    ~CharBuffer();
+
+ public:
+    char* buffer() override      { return buf; }
+    int   size() override        { return len; }
+
+ private:
+    char* buf = nullptr;
+    int   len = 0;
+};
+
+}
+
+#endif //SPS_BUFFER_HPP
