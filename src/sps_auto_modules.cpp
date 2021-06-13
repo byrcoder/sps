bb969951b3e8d7aba103b6ab7a09ef3f1e141290
weideng <deare1300@163.com>
2021-06-13 23:22:12 +0800

    增加stream模块

--- src/sps_auto_modules.cpp
+++ src/sps_auto_modules.cpp
@@ -9,16 +9,21 @@
 
 #include <app/server/sps_server_config.hpp>
 
+#include <app/stream/sps_stream_config.hpp>
+
 #include <app/upstream/sps_upstream_config.hpp>
 
 #define MODULE_INSTANCE(NAME) (std::make_shared<sps::NAME##ModuleFactory>())
 
 sps::PIModuleFactory modules[] = {
     MODULE_INSTANCE(Core),
-    MODULE_INSTANCE(Host),
-    MODULE_INSTANCE(Location),
+
     MODULE_INSTANCE(Http),
     MODULE_INSTANCE(Server),
+    MODULE_INSTANCE(Host),
+    MODULE_INSTANCE(Location),
+    MODULE_INSTANCE(Stream),
+
     MODULE_INSTANCE(UpStream),
     nullptr
 };
